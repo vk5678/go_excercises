ce6aa331e00e7a4675503af5aceeeb6f7fbe5a13
manikanta <manikantavinodkumar888@gmail.com>
2019-12-06 20:55:12 +0530

    second commit

--- handler/handler.go
+++ handler/handler.go
@@ -3,7 +3,7 @@ package handler
 import (
 	"net/http"
 )
-
+//this file is present in handler package which is imported by our main package which has our main.go file"
 //exported function for handling the web requests of key value type
 func MapHandler(pathToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 
